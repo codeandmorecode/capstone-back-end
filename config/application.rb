6cb46dbd24f40ef0c46ef841954002c6a1d31aa9
Alice D <alicedilanchian@gmail.com>
2021-02-04 18:20:07 -0500

    app created; keys set

---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module CapstoneBackEnd
+  class Application < Rails::Application
+  config.generators do |g|
+    # Force new test files to be generated in the minitest-spec style
+    g.test_framework :minitest, spec: true
+  end
+  config.add_autoload_paths_to_load_path = true
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
